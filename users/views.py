73c33b50c3a589898fffc1c9846f9afdd635b58c
Paul <lumteshi@gmail.com>
2020-10-01 21:29:27 +0000

    Added captcha re-capatcha

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+import json
+import urllib
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.urls import reverse_lazy
@@ -61,6 +63,21 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
+            
+            ''' Begin reCAPTCHA validation '''
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            values = {
+                'secret': settings.RECAPTCHA_PRIVATE_KEY,
+                'response': recaptcha_response
+            }
+            data = urllib.parse.urlencode(values).encode()
+            req = urllib.request.Request(url, data=data)
+            response = urllib.request.urlopen(req)
+            result = json.loads(response.read().decode())
+            ''' End reCAPTCHA validation '''
+            
+            
             user = form.save(commit=False)
             user.is_active = False
             user.is_staff = True
@@ -87,6 +104,7 @@ def register(request):
                 request, ('Account created successfully. Please go to your email to verify your account.'))
             return redirect('bookings-home')
         else:
+            messages.error(request, 'Invalid reCAPTCHA. Please try again.')
             print(form.errors)
     else:
         form = RegistrationForm()
